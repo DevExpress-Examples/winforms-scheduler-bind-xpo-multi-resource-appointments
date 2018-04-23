3249a2719c33d690222446f19056af5733231729
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.Xpo;
+using DevExpress.XtraScheduler;
+
+namespace XPO_Bound_Multiresource {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void OnAppointmentsChanged(object sender, PersistentObjectsEventArgs e) {
+            foreach (Appointment apt in e.Objects) {
+                XPBaseObject o = apt.GetSourceObject(schedulerStorage1) as XPBaseObject;
+                if (o != null)
+                    o.Save();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            ResourceBaseCollection resources = schedulerStorage1.Resources.Items;
+            if (resources.Count <= 0) {
+                resources.Add(new Resource(0, "Andrew Fuller"));
+                resources.Add(new Resource(1, "Nancy Davolio"));
+                resources.Add(new Resource(2, "Janet Leverling"));
+                resources.Add(new Resource(3, "Margaret Peacock"));
+            }
+            int count = xpCollectionResources.Count;
+            for (int i = 0; i < count; i++) {
+                XPObject o = (XPObject)xpCollectionResources[i];
+                o.Save();
+            }
+        }
+
+    }
+}
---
+++ VB/Form1.vb
@@ -0,0 +1,39 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Forms
+Imports DevExpress.Xpo
+Imports DevExpress.XtraScheduler
+
+Namespace XPO_Bound_Multiresource
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub OnAppointmentsChanged(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs) Handles schedulerStorage1.AppointmentsInserted, schedulerStorage1.AppointmentsChanged
+			For Each apt As Appointment In e.Objects
+				Dim o As XPBaseObject = TryCast(apt.GetSourceObject(schedulerStorage1), XPBaseObject)
+				If o IsNot Nothing Then
+					o.Save()
+				End If
+			Next apt
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+			Dim resources As ResourceBaseCollection = schedulerStorage1.Resources.Items
+			If resources.Count <= 0 Then
+				resources.Add(New Resource(0, "Andrew Fuller"))
+				resources.Add(New Resource(1, "Nancy Davolio"))
+				resources.Add(New Resource(2, "Janet Leverling"))
+				resources.Add(New Resource(3, "Margaret Peacock"))
+			End If
+			Dim count As Integer = xpCollectionResources.Count
+			For i As Integer = 0 To count - 1
+				Dim o As XPObject = CType(xpCollectionResources(i), XPObject)
+				o.Save()
+			Next i
+		End Sub
+
+	End Class
+End Namespace
