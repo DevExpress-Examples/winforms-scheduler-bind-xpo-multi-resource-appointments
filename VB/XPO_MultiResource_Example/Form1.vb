d4c1afbb65cbd71d89e406f4feaed5d258fdb2ab
Lex EPoX <13219327+LexEPoX@users.noreply.github.com>
2018-10-08 19:59:40 +0300

    SchedulerStorage -> SchedulerDataStorage

--- VB/XPO_MultiResource_Example/Form1.vb
+++ VB/XPO_MultiResource_Example/Form1.vb
@@ -14,54 +14,54 @@ Namespace XPO_MultiResource_Example
 
 			session1.ConnectionString = DevExpress.Xpo.DB.AccessConnectionProvider.GetConnectionString("XPO_MultiResource_Example.mdb")
 
-			schedulerStorage1.Appointments.DataSource = xpCollectionAppointments
-			schedulerStorage1.Resources.DataSource = xpCollectionResources
+            schedulerDataStorage1.Appointments.DataSource = xpCollectionAppointments
+            schedulerDataStorage1.Resources.DataSource = xpCollectionResources
 
-			CreateMappings()
-			InitResources()
+            CreateMappings()
+            InitResources()
 
-			AddHandler Me.schedulerStorage1.AppointmentsChanged, AddressOf schedulerStorage1_AppointmentsChanged
-			AddHandler Me.schedulerStorage1.AppointmentsInserted, AddressOf schedulerStorage1_AppointmentsChanged
+            AddHandler Me.schedulerDataStorage1.AppointmentsChanged, AddressOf schedulerDataStorage1_AppointmentsChanged
+            AddHandler Me.schedulerDataStorage1.AppointmentsInserted, AddressOf schedulerDataStorage1_AppointmentsChanged
 
-			Me.schedulerStorage1.Appointments.ResourceSharing = True
-			Me.schedulerControl1.Views.DayView.GroupType = SchedulerGroupType.Resource
-		End Sub
 
-		Private Sub CreateMappings()
-			Me.schedulerStorage1.Appointments.Mappings.AllDay = "AllDay"
-			Me.schedulerStorage1.Appointments.Mappings.Description = "Description"
-			Me.schedulerStorage1.Appointments.Mappings.End = "Finish"
-			Me.schedulerStorage1.Appointments.Mappings.Label = "Label"
-			Me.schedulerStorage1.Appointments.Mappings.Location = "Location"
-			Me.schedulerStorage1.Appointments.Mappings.RecurrenceInfo = "Recurrence"
-			Me.schedulerStorage1.Appointments.Mappings.ReminderInfo = "Reminder"
-			Me.schedulerStorage1.Appointments.Mappings.ResourceId = "ResourceIds"
-			Me.schedulerStorage1.Appointments.Mappings.Start = "Created"
-			Me.schedulerStorage1.Appointments.Mappings.Status = "Status"
-			Me.schedulerStorage1.Appointments.Mappings.Subject = "Subject"
-			Me.schedulerStorage1.Appointments.Mappings.Type = "AppointmentType"
+            Me.schedulerControl1.Views.DayView.GroupType = SchedulerGroupType.Resource
+        End Sub
 
-			Me.schedulerStorage1.Resources.Mappings.Caption = "Name"
-			Me.schedulerStorage1.Resources.Mappings.Color = "Color"
-			Me.schedulerStorage1.Resources.Mappings.Id = "ResId"
-			Me.schedulerStorage1.Resources.Mappings.Image = "Image"
-		End Sub
+        Private Sub CreateMappings()
+            Me.schedulerDataStorage1.Appointments.Mappings.AllDay = "AllDay"
+            Me.schedulerDataStorage1.Appointments.Mappings.Description = "Description"
+            Me.schedulerDataStorage1.Appointments.Mappings.End = "Finish"
+            Me.schedulerDataStorage1.Appointments.Mappings.Label = "Label"
+            Me.schedulerDataStorage1.Appointments.Mappings.Location = "Location"
+            Me.schedulerDataStorage1.Appointments.Mappings.RecurrenceInfo = "Recurrence"
+            Me.schedulerDataStorage1.Appointments.Mappings.ReminderInfo = "Reminder"
+            Me.schedulerDataStorage1.Appointments.Mappings.ResourceId = "ResourceIds"
+            Me.schedulerDataStorage1.Appointments.Mappings.Start = "Created"
+            Me.schedulerDataStorage1.Appointments.Mappings.Status = "Status"
+            Me.schedulerDataStorage1.Appointments.Mappings.Subject = "Subject"
+            Me.schedulerDataStorage1.Appointments.Mappings.Type = "AppointmentType"
 
-		Private Sub InitResources()
-			If xpCollectionResources.Count <= 0 Then
-				For i As Integer = 0 To resourceNames.Length - 1
-                    Dim res As Resource = schedulerStorage1.Resources.CreateResource(i)
+            Me.schedulerDataStorage1.Resources.Mappings.Caption = "Name"
+            Me.schedulerDataStorage1.Resources.Mappings.Color = "Color"
+            Me.schedulerDataStorage1.Resources.Mappings.Id = "ResId"
+            Me.schedulerDataStorage1.Resources.Mappings.Image = "Image"
+        End Sub
+
+        Private Sub InitResources()
+            If xpCollectionResources.Count <= 0 Then
+                For i As Integer = 0 To resourceNames.Length - 1
+                    Dim res As Resource = schedulerDataStorage1.Resources.CreateResource(i)
                     res.Caption = resourceNames(i)
-					res.ColorValue = resourceColors(i)
-					schedulerStorage1.Resources.Add(res)
-				Next i
-				session1.Save(xpCollectionResources)
-			End If
-		End Sub
+                    res.ColorValue = resourceColors(i)
+                    schedulerDataStorage1.Resources.Add(res)
+                Next i
+                session1.Save(xpCollectionResources)
+            End If
+        End Sub
 
-		Private Sub schedulerStorage1_AppointmentsChanged(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs)
+        Private Sub schedulerDataStorage1_AppointmentsChanged(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs)
             For Each apt As Appointment In e.Objects
-                Dim o As XPBaseObject = TryCast(apt.GetSourceObject(schedulerStorage1), XPBaseObject)
+                Dim o As XPBaseObject = TryCast(apt.GetSourceObject(schedulerDataStorage1), XPBaseObject)
                 If o IsNot Nothing Then
                     o.Save()
                 End If
